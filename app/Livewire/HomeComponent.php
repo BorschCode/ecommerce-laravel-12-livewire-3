5882f0e8429f933026f76c4e7da3085506330dd1
serega <serega@local.tuf>
2026-01-05 17:12:45 +0200

    🇨🇿     lesson 5

--- app/Livewire/HomeComponent.php
+++ app/Livewire/HomeComponent.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class HomeComponent extends Component
 {
-    public function render()
+    public function render(): View
     {
-        return view('livewire.home-component');
+        $hits_products = Product::whereIsHit(true)->orderBy('id', 'desc')->limit(4)->get();
+        $new_products = Product::whereIsNew(true)->orderBy('id', 'desc')->limit(8)->get();
+        return view('livewire.home-component', [
+            'hits_products' => $hits_products,
+            'new_products' => $new_products
+        ]);
     }
 }
